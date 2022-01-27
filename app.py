b330a689b49b1ddffd93535bc9b38fc0cd7df671
enrique-zarate <eszg.fury@gmail.com>
2022-01-27 14:26:15 -0300

    app.py para logica con LEDs

---
+++ app.py
@@ -0,0 +1,17 @@
+
+
+def lights_validation(state):
+    '''
+    Logic for turning lights depending on user's credentials validation 
+    '''
+
+    return None
+
+
+
+
+if __name__ == "__main__":
+
+    state = input("Ingrese estado")
+
+    lights_validation(state)
